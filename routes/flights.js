71d0c7ce68f46326c94c7b3981e0f62997ca9a82
Charles MacMillan <charlesmacmillanpro@gmail.com>
2020-08-21 17:46:31 -0600

    base styling and HTML table  in views/flights/index

---
+++ routes/flights.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const flightsCtrl = require('../controllers/flights');
+
+/* GET users listing. */
+router.get('/', flightsCtrl.index); 
+
+module.exports = router;
